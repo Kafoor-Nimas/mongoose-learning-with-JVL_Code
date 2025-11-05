e370acb142ffb33504f2ce6b9990732c8cdb5ff0
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-05 15:59:33 +0530

    Schema

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const User = require("./models/userModel");
+
 mongoose
   .connect("mongodb://localhost:27017/jvlcode")
   .then(() => {
@@ -7,3 +9,18 @@ mongoose
   .catch(() => {
     console.log("connection error");
   });
+
+const user = new User({
+  name: "Logesh",
+  age: 28,
+});
+// user.save().then(() => {
+//   console.log("User Saved");
+// });
+
+async function run() {
+  const newUser = await user.save();
+  console.log(newUser);
+}
+
+run()
